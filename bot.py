21bf53465aabbd877ed06827f57bc86f534af58e
Nikolai Suslow <90977134+niko2s@users.noreply.github.com>
2023-09-23 17:06:24 +0200

    add alternative dictionary command

--- bot.py
+++ bot.py
@@ -103,6 +103,57 @@ async def say(interaction: discord.Interaction, thing_to_say: str):
     await interaction.channel.send("Hello")
 
 
+@bot.tree.command(name="urban")
+@app_commands.describe(search="What word should I look up?")
+async def urban(interaction: discord.Interaction, search: str = None):
+    url = os.getenv("URBAN_URL")
+
+    querystring = None
+    
+    #if no word was given, look up random word
+    if search:
+        url += "define"
+        querystring = {"term": search}
+    else:
+        url += "random"
+
+    headers = {
+        "X-RapidAPI-Key": os.getenv("URBAN_KEY"),
+        "X-RapidAPI-Host": os.getenv("URBAN_HOST"),
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    if response.status_code == 200:
+        data = response.json()
+        results = data["list"]
+
+        result = results[0]
+
+        title = result["word"]
+        if search is None:
+            title = "Random word: " + title
+        description = result["definition"]
+        example = result["example"]
+        thumbs_up = result["thumbs_up"]
+        thumbs_down = result["thumbs_down"]
+        permalink = result["permalink"]
+
+        embed = discord.Embed(
+            title=f"{title}", description=description, color=discord.Color.from_str("0xDD0000")
+        )
+
+        embed.add_field(name="Example", value=example, inline=False)
+        embed.add_field(name=":thumbsup:", value=thumbs_up)
+        embed.add_field(name=":thumbsdown:", value=thumbs_down)
+        
+        embed.set_footer(text=permalink)
+
+        await interaction.response.send_message(embed=embed)
+    else:
+        await interaction.response.send_message("Error happened! Status code" + response.status_code)
+        
+
 @bot.tree.command(name="word")
 @app_commands.describe(search="What word should I look up?")
 async def word(interaction: discord.Interaction, search: str):
@@ -122,7 +173,7 @@ async def word(interaction: discord.Interaction, search: str):
         embed = discord.Embed(
             title=f"{title}",
             description=description,
-            color=discord.Color.from_str("0x00CC00")
+            color=discord.Color.from_str("0x00CC00"),
         )
 
         for phonetic in phonetics:
@@ -162,7 +213,7 @@ async def word(interaction: discord.Interaction, search: str):
     elif response.status_code == 404:
         await interaction.response.send_message(f"Could not find the word `{search}`")
     else:
-        #add error logging
+        # add error logging
         await interaction.response.send_message("An error occured. Try again later.")
 
 
