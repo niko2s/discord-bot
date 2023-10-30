6326f56e9ef687990689a00522faaa58fdd10c1c
Nikolai Suslow <90977134+niko2s@users.noreply.github.com>
2023-10-30 23:44:39 +0100

    Add author and prompt to message in AI command and correct usage of defer, followup

--- cogs/ai.py
+++ cogs/ai.py
@@ -14,11 +14,7 @@ class ai(commands.Cog):
     @app_commands.command(name="ai", description="Chat with AI!")
     @app_commands.describe(prompt="Enter a message")
     async def ai(self, interaction: discord.Interaction, prompt: str):
-        """Chat with AI
-
-        Args:
-            ctx (commands.Context): _description_
-        """
+        """Chat with AI"""
         await interaction.response.defer(thinking=True)
         result = g4f.ChatCompletion.create(
             model="gpt-3.5-turbo",
@@ -28,14 +24,13 @@ class ai(commands.Cog):
             ],
         )
 
-        msg = await interaction.original_response()
+        follow_up: discord.Webhook = interaction.followup
+
+        result = f"{interaction.user.mention}: `{prompt}`\n---------------------------------\n" + result
 
         for i in range(0, len(result), MAX_MESSAGE_LENGTH):
-            chunk = result[i : i + MAX_MESSAGE_LENGTH]
-            if i == 0:
-                await msg.edit(content=chunk)
-            else:
-                await interaction.channel.send(content=chunk)
+            chunk = result[i: i + MAX_MESSAGE_LENGTH]
+            await follow_up.send(content=chunk)
 
 
 async def setup(client: commands.Bot) -> None:
