4b8bdd3fe7719d9d7b5c92964f20bbdbea27d179
Nikolai Suslow <90977134+niko2s@users.noreply.github.com>
2024-02-24 20:59:48 +0100

    Update requirements and ai

--- cogs/ai.py
+++ cogs/ai.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import g4f
+from g4f.client import Client
 import os
 
 MAX_MESSAGE_LENGTH = 2000  # characters
@@ -10,23 +11,25 @@ MAX_MESSAGE_LENGTH = 2000  # characters
 class ai(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
+        self.gpt = Client() 
+
 
     @app_commands.command(name="ai", description="Chat with AI!")
     @app_commands.describe(prompt="Enter a message")
     async def ai(self, interaction: discord.Interaction, prompt: str):
         """Chat with AI"""
         await interaction.response.defer(thinking=True)
-        result = g4f.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            provider=g4f.Provider.GptGo,
+        result = self.gpt.chat.completions.create(
+            provider=g4f.Provider.Bing,
+            model="gpt-4",
             messages=[
                 {"role": "user", "content": os.environ.get("PRE_PROMPT") + prompt}
-            ],
+            ]
         )
 
         follow_up: discord.Webhook = interaction.followup
 
-        result = f"{interaction.user.mention}: `{prompt}`\n---------------------------------\n" + result
+        result = f"{interaction.user.mention}: `{prompt}`\n---------------------------------\n" + result.choices[0].message.content
 
         for i in range(0, len(result), MAX_MESSAGE_LENGTH):
             chunk = result[i: i + MAX_MESSAGE_LENGTH]
