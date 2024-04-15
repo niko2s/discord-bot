331ed8955727b568b01b78511ac598656ab5687a
Nikolai Suslow <90977134+niko2s@users.noreply.github.com>
2024-04-15 23:18:03 +0200

    fix linting cogs/cat.py

--- cogs/cat.py
+++ cogs/cat.py
@@ -1,10 +1,10 @@
+import os
+import logging
+import requests
 import discord
 from discord.ext import commands
 from discord import app_commands
 from utils import download
-import logging
-import requests
-import os
 
 
 class Cat(commands.Cog):
@@ -13,13 +13,17 @@ class Cat(commands.Cog):
 
     @app_commands.command(name="cat", description="Sends random cat picture!")
     async def cat(self, interaction: discord.Interaction):
-        response = requests.get(os.environ.get("CAT_API"))
+        response = requests.get(os.environ.get("CAT_API"), timeout=10)
         if response.status_code == 200:
             data = response.json()[0]
             file_data = await download.download_file(data["url"])
-            await interaction.response.send_message(file=discord.File(file_data, "cat.jpg"))
+            await interaction.response.send_message(
+                file=discord.File(file_data, "cat.jpg")
+            )
         else:
-            logging.error(f"Error at cat command. response status code: {response.status_code}")
+            logging.error(
+                "Error at cat command. response status code: %s", response.status_code
+            )
             await interaction.response.send_message("Failed to retrieve cat picture")
 
 
