c7e8d665467fd35f3df4a9a90e314d0827800f2a
Nikolai Suslow <90977134+niko2s@users.noreply.github.com>
2024-04-15 22:55:05 +0200

    fix linting bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
-import discord
-from discord.ext import commands
+"""
+This module defines the Client class for the Discord bot.
+It extends the commands.Bot class from discord.py, loads commands from cogs and logs in as the bot.
+And provides an entry point to start the bot.
+"""
+
 import logging
 import os
+import discord
+from discord.ext import commands
 import nest_asyncio
-from datetime import datetime
 from dotenv import load_dotenv, find_dotenv
-import sys
+
 
 class Client(commands.Bot):
     def __init__(self):
@@ -21,15 +26,14 @@ class Client(commands.Bot):
                 await self.load_extension(f"cogs.{name}")
 
     async def on_ready(self):
-        logging.info(f"Logged on as {self.user}!")
+        logging.info("Logged on as %s!", self.user)
 
         nest_asyncio.apply()
         synced = await self.tree.sync()
-        logging.info(f"Synced {len(synced)} command(s)")
+        logging.info("Synced %s command(s)", len(synced))
 
+if __name__ == "__main__":
+    load_dotenv(find_dotenv(), override=True)
 
-load_dotenv(find_dotenv(), override=True)
-                          
-
-client = Client()
-client.run(os.environ.get("DISCORD_TOKEN"), root_logger=True)
+    client = Client()
+    client.run(os.environ.get("DISCORD_TOKEN"), root_logger=True)
