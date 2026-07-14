da571da70d5a537468645ba83454ad4e1a766c4a
Nikolai <90977134+niko2s@users.noreply.github.com>
2026-07-14 21:36:26 +0200

    feat: add Lavalink music playback (#39)
    * feat: add music cog with Lavalink integration
    
    * feat: route Lavalink through NordVPN
    
    * fix: remove unused Lavalink OAuth placeholder
    
    * fix: improve music playback messaging
    
    * docs and remove useless

---
+++ terraform/backend.hcl
@@ -0,0 +1,4 @@
+bucket       = "discord-bot-tfstate-485044328876"
+key          = "discord-bot/terraform.tfstate"
+region       = "eu-central-1"
+use_lockfile = true
--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -3,3 +3,9 @@ resource "aws_secretsmanager_secret" "bot" {
   description             = "Runtime secrets (DISCORD_TOKEN, OPENAI_*, URBAN_KEY, ...)."
   recovery_window_in_days = 0
 }
+
+resource "aws_secretsmanager_secret" "nordvpn" {
+  name                    = "${var.name}/nordvpn"
+  description             = "NordVPN OpenVPN service credentials for the Lavalink VPN."
+  recovery_window_in_days = 0
+}
